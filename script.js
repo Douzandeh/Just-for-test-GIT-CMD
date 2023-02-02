f5d042947c660e58c88a5be8504d7bdf696cdd17
Hossein Douzandeh <Douzandeh.k@email.come>
2023-02-02 18:39:47 +0330

    This is test for git Logical operator

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 // let str = "324";
 // let num = Number(str);
 // alert(typeof num)
-let value = true;
-alert(typeof value); // boolean
+// let value = true;
+// alert(typeof value); // boolean
 
-value = String(value); // now value is a string "true"
-alert(typeof value); // string
-alert(Boolean(0)); // false
-alert(Boolean(1)); // true
-alert(Boolean("")) // false
-alert(Boolean(" ")) // true
+// value = String(value); // now value is a string "true"
+// alert(typeof value); // string
+// alert(Boolean(0)); // false
+// alert(Boolean(1)); // true
+// alert(Boolean("")) // false
+// alert(Boolean(" ")) // true
+
+let firstNumber = 5;
+
+let secondNumber = 6;
+
+let result = firstNumber + secondNumber;
+
+let minusResult = firstNumber - secondNumber;
+
+let zarbResult = firstNumber * secondNumber;
+
+alert(zarbResult)
