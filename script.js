65580b11c7e9d125d46d33645f82514dfb21763f
Hossein Douzandeh <Douzandeh.k@email.come>
2023-02-02 15:43:56 +0330

    This is branche 01 Boolean

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-let str = "324";
-let num = Number(str);
-alert(typeof num)
+// let str = "324";
+// let num = Number(str);
+// alert(typeof num)
+let value = true;
+alert(typeof value); // boolean
+
+value = String(value); // now value is a string "true"
+alert(typeof value); // string
+alert(Boolean(0)); // false
+alert(Boolean(1)); // true
+alert(Boolean("")) // false
+alert(Boolean(" ")) // true
